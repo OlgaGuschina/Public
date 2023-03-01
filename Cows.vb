7f5f9fcf4e733226e277bf1971e10fba7d91917b
OlgaGuschina <80212232+OlgaGuschina@users.noreply.github.com>
2023-03-01 08:48:48 +0300

    Add files via upload

---
+++ Cows.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+
+Module Cows
+    Sub Main(args As String())
+        Dim number, ver As String
+        'TODO: генерируем число
+        number = "1234"
+
+        Console.WriteLine("Угадай число")
+
+        Do
+            ver = Console.ReadLine()
+            For i = 0 To 3
+                If (number.Chars(0) = ver.Chars(0)) Then
+                    Console.WriteLine("Угадал")
+                Else
+                    Console.WriteLine("Не верно")
+                End If
+            Next
+        Loop Until number = ver
+    End Sub
+End Module
